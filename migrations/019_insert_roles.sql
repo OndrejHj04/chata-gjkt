20710c387c5ceb852846536518fbb745f461cd2e
Ondřej Hájek <ondrej.hajek.profi@gmail.com>
2025-08-04 22:51:51 +0200

    fix

---
+++ migrations/019_insert_roles.sql
@@ -0,0 +1,5 @@
+INSERT INTO `roles` (`id`, `name`) VALUES
+(1, 'admin'),
+(2, 'správce'),
+(3, 'uživatel'),
+(4, 'veřejnost');
---
+++ migrations/020_insert_user.sql
@@ -0,0 +1,2 @@
+INSERT INTO `users` (`first_name`, `last_name`, `role`, `password`, `email`, `verified`, `theme`, `children`) 
+VALUES ('Test', 'User', 1, MD5('meinestadt'), 'ondrej.hajek.profi@gmail.com', 1, 1, 0);
