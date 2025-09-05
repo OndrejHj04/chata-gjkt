1a826314d3e44fd5fb17206aba50e4c0d6607d5e
Ondřej Hájek <ondrej.hajek.profi@gmail.com>
2025-09-05 14:09:51 +0200

    bump

--- lib/db.ts
+++ lib/db.ts
@@ -1,29 +1,30 @@
 import mysql from "mysql2/promise";
 
-export async function getConnection(){
+export async function getConnection() {
   return await mysql.createConnection({
     host: process.env.MYSQL_HOST,
     database: process.env.MYSQL_DATABASE,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    multipleStatements: true
+    multipleStatements: true,
   });
 }
 
-export async function query({
+export async function query<T extends any>({
   query,
   values = [],
 }: {
   query: string;
   values?: any[];
-}) {
-  const connection = await getConnection()
+}): Promise<T> {
+  const connection = await getConnection();
 
   try {
     const [results] = await connection.query(query, values);
-    connection.end()
-    return results;
-  } catch (error: any) {
-    throw error
+    await connection.end();
+    return results as T
+  } catch (error) {
+    await connection.end();
+    throw error;
   }
 }
